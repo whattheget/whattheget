23a8cee3c5b15eb13e493b4d35277b5cdf36e186
whattheget <dragoneugene18@gmail.com>
2025-07-17 17:49:21 -0700

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -14,7 +14,6 @@ local function downloadFile(path, func)
 			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPNG/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
-			error(res)
 		end
 		if path:find('.lua') then
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
